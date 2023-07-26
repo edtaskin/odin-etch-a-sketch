00da39a6500178e2ce621cb37e0d827b0a6101e1
edtaskin <edtaskin2002@gmail.com>
2023-07-26 18:47:23 +0300

    Add functionality to pen and eraser

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const gridDiv = document.querySelector(".grid-container");
 const gridArray = [];
 let gridDimension = 16;
 const getCellSize = () => SKETCH_CONTAINER_SIZE / gridDimension; 
+let penColor = "black";
 
 function drawGrid(gridDimension) {
     const cellSize = getCellSize();
-    console.log(cellSize);
     for (let i = 0; i < gridDimension; i++) {
         for (let j = 0; j < gridDimension; j++) {
             const cellDiv = document.createElement('div');
@@ -18,9 +18,33 @@ function drawGrid(gridDimension) {
             cellDiv.classList.add("cell");
             gridArray.push(cellDiv);
             gridDiv.appendChild(cellDiv);
+
+            cellDiv.addEventListener('click', () => {
+                penColor = colorSelector.value;
+                cellDiv.style.backgroundColor = penColor;
+            });
         }
     }
 }
+
+
+const colorSelector = document.querySelector("input#color-selector");
+const eraserButton = document.querySelector(".bt#eraser");
+let previousColor = colorSelector.value;
+eraserButton.addEventListener('click', () => {
+    console.log(previousColor);
+    eraserButton.classList.toggle("toggle-on");
+    if (eraserButton.classList.contains("toggle-on")) {
+        previousColor = colorSelector.value;
+        penColor = "#ffffff";
+        colorSelector.value = "#ffffff"
+    }
+    else {
+        colorSelector.value = previousColor;
+        penColor = previousColor;
+    }
+});
+
 
 
 drawGrid(gridDimension);
